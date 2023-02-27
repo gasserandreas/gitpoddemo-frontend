ca5d05ad1469e8b31c93967c87d41ba77eb34988
Andreas Liistro <gasserandreas@users.noreply.github.com>
2023-02-27 13:53:33 +0000

    task: added about page content

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Typography from '@mui/material/Typography';
+
 const About = () => (
-  <div>About</div>
+  <div>
+    <Typography variant="h5">About this page</Typography>
+    <Typography>Simple test frontend application to demo usage of GitPod. ;-)</Typography>
+  </div>
 );
 
 export default About;
